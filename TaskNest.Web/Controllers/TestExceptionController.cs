e0db7e2fedeb902c886d172169408595f7685729
Tshepo <Tshepon@blackmagic.co.za>
2025-09-14 19:41:05 +0200

    add a little message

--- TaskNest.Web/Controllers/TestExceptionController.cs
+++ TaskNest.Web/Controllers/TestExceptionController.cs
@@ -4,6 +4,7 @@
     [Route("api/[controller]")]
     public class TestExceptionController : ControllerBase
     {
+        // just testing
         [HttpGet("throw")]
         public IActionResult Throw()
         {
